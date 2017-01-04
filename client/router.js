88a1e1061be4c5b3067dee6841c49454b0e2514c
bkd705 <comparison@live.com>
2017-01-04 03:49:07 -0500

    Added placeholder requireAuth function aswell as base dashboard page

--- client/router.js
+++ client/router.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Router, Route, browserHistory, IndexRoute } from 'react-router'
+import requireAuth from './utils/requireAuth'
 
 // Layouts
 import MainLayout from './layouts/MainLayout'
@@ -8,6 +9,7 @@ import MainLayout from './layouts/MainLayout'
 import Home from './pages/Index'
 import SignUp from './pages/SignUp'
 import Login from './pages/Login'
+import Dashboard from './pages/Dashboard'
 
 export default (
 	<Router history={browserHistory}>
@@ -16,6 +18,7 @@ export default (
 			<Route path='/' component={Home} />
 			<Route path='/login' component={Login} />
 			<Route path='/signup' component={SignUp} />
+			<Route path='/dashboard' component={Dashboard} onEnter={requireAuth} />
 		</Route>
 
 	</Router>
